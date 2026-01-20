26d512048355103db0719a4fa67148c8c1dcace4
shanebrannick <shanebrannick@yahoo.com>
2026-01-20 14:13:52 +0000

    Added SSR

--- apps/angular-demo/src/app/app.config.ts
+++ apps/angular-demo/src/app/app.config.ts
@@ -5,9 +5,14 @@ import {
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import {
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideClientHydration(withEventReplay()),
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
