6117af3cb6a88f6c879b3a6bd36e5cf06d5e0a9d
shanebrannick <shanebrannick@gmail.com>
2026-01-21 19:17:35 +0000

    Fixed lint ande tests

--- libs/@angular-demo/web/@entry-point/src/lib/app.spec.ts
+++ libs/@angular-demo/web/@entry-point/src/lib/app.spec.ts
@@ -13,8 +13,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome angular-demo'
-    );
+    expect(compiled.querySelector('router-outlet')).toBeTruthy();
   });
 });
